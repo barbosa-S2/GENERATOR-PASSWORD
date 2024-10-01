4ba2fbcd9eae750cef1621ea3b6a7063cd837f14
GuiBarbosa <137212127+barbosa-S2@users.noreply.github.com>
2024-10-01 09:34:52 -0300

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,1 +1,21 @@
+import random
+
+print('=====Seja Bem Vindo ao Generator Password=====')
+
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&*().,;'
+
+number = input('Quantia de senhas para serem geradas: ')
+number = int(number)
+
+length = input('Quantos caracteres devem ter suas senhas: ')
+length = int(length)
+
+print('\nEstas são Senhas: ')
+ 
+for pwd in  range(number):
+    passwords = ''
+    for c in range(length):
+        passwords += random.choice(chars)
+    print(passwords)
+
 
